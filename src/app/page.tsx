84a9dfd987e18b122e57af51eabb648bbf37367e
AntDavi <ant.davi07@gmail.com>
2025-09-12 09:43:10 -0300

    initial commit and setup of the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,110 @@
+import React from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { HeroHeader } from "@/components/header";
 import Image from "next/image";
 
-export default function Home() {
+export default function HeroSection() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <HeroHeader />
+      <main className="[--color-primary:var(--color-indigo-500)]">
+        <section className="overflow-hidden">
+          <div className="py-20 md:py-36">
+            <div className="relative z-10 mx-auto max-w-5xl px-6">
+              <div className="relative text-center">
+                <h1 className="mx-auto max-w-2xl text-balance text-4xl font-bold md:text-5xl">
+                  Construa seu MicroSaaS com agilidade e qualidade
+                </h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+                <p className="text-muted-foreground mx-auto my-6 max-w-2xl text-balance text-xl">
+                  Nextheus é o boilerplate ideal para criar, lançar e escalar
+                  aplicações SaaS modernas com suporte a IA, autenticação e
+                  muito mais.
+                </p>
+
+                <div className="flex flex-col items-center justify-center gap-3 *:w-full sm:flex-row sm:*:w-auto">
+                  <Button asChild size="lg">
+                    <Link href="#link">
+                      <span className="text-nowrap">Comece Agora</span>
+                    </Link>
+                  </Button>
+                  <Button key={2} asChild size="lg" variant="outline">
+                    <Link href="#link">
+                      <span className="text-nowrap">Ver Demonstração</span>
+                    </Link>
+                  </Button>
+                </div>
+              </div>
+
+              <div className="relative mx-auto mt-12 max-w-5xl overflow-hidden rounded-3xl bg-black/10 md:mt-20">
+                <Image
+                  src="/forest.jpg"
+                  alt="background"
+                  className="absolute inset-0 size-full object-cover"
+                  width="2942"
+                  height="1651"
+                />
+
+                <div className="bg-background rounded-(--radius) relative m-4 overflow-hidden border border-transparent shadow-xl shadow-black/15 ring-1 ring-black/10 sm:m-8 md:m-12">
+                  <Image
+                    src="/home.png"
+                    alt="app screen"
+                    width="2880"
+                    height="1842"
+                    className="object-top-left size-full object-cover"
+                  />
+                </div>
+              </div>
+
+              {/* <div className="mt-8">
+                <p className="text-muted-foreground text-center">
+                  Trusted by teams at :
+                </p>
+                <div className="mt-4 flex items-center justify-center gap-12">
+                  <div className="flex">
+                    <img
+                      className="mx-auto h-5 w-fit"
+                      src="https://html.tailus.io/blocks/customers/nvidia.svg"
+                      alt="Nvidia Logo"
+                      height="20"
+                      width="auto"
+                    />
+                  </div>
+
+                  <div className="flex">
+                    <img
+                      className="mx-auto h-4 w-fit"
+                      src="https://html.tailus.io/blocks/customers/column.svg"
+                      alt="Column Logo"
+                      height="16"
+                      width="auto"
+                    />
+                  </div>
+                  <div className="flex">
+                    <img
+                      className="mx-auto h-4 w-fit"
+                      src="https://html.tailus.io/blocks/customers/github.svg"
+                      alt="GitHub Logo"
+                      height="16"
+                      width="auto"
+                    />
+                  </div>
+                  <div className="flex">
+                    <img
+                      className="mx-auto h-5 w-fit"
+                      src="https://html.tailus.io/blocks/customers/nike.svg"
+                      alt="Nike Logo"
+                      height="20"
+                      width="auto"
+                    />
+                  </div>
+                </div>
+              </div> */}
+            </div>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
---
+++ src/components/logo-icon.tsx
@@ -0,0 +1,10 @@
+import { SparkleIcon } from "lucide-react";
+
+export default function LogoIcon() {
+  return (
+    <div className="flex items-center space-x-2">
+      <SparkleIcon />
+      <span className="font-bold">Nextheus</span>
+    </div>
+  );
+}
